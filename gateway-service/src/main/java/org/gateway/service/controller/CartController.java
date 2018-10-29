ea078b865d2c72444fc0849902e3456937b34ba7
weibinyu <weibinyu@users.noreply.github.com>
2018-10-30 02:38:27 +0800

    Update cartControll

--- gateway-service/src/main/java/org/gateway/service/controller/CartController.java
+++ gateway-service/src/main/java/org/gateway/service/controller/CartController.java
@@ -2,7 +2,6 @@ package org.gateway.service.controller;
 
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -12,28 +11,36 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CartController {
-	  @Autowired
-      ApplicationArguments appArgs;
-	  
-	  
-	  @Bean
-	  public RouteLocator cartRoutes(RouteLocatorBuilder builder) {
-		  List<String> arg = appArgs.getOptionValues("ip");  
-		  
-		  return builder.routes()
-			   .route(p -> p
-			           	.path("/carts")
-			           	.uri("http://"+arg.get(0)+":8081"))
-			   .route(p -> p
-			           	.path("/carts/products")
-			           	.uri("http://"+arg.get(0)+":8081"))
-			   .route(p -> p
-			           	.path("/carts/**/products/**")
-			           	.uri("http://"+arg.get(0)+":8081"))
-		       .route(p -> p
-		    		   .method("GET").and()
-		               .path("/carts/**")
-		               .uri("http://"+arg.get(0)+":8082"))
-		       .build();
-	   }
+	@Autowired
+	ApplicationArguments appArgs;
+	
+
+	@Bean
+	public RouteLocator cartRoutes(RouteLocatorBuilder builder) {
+		List<String> arg = appArgs.getOptionValues("ip");  
+
+		return builder.routes()
+				.route(p -> p
+						.path("/get_cart_by_ID/**")
+						.and().method("GET")
+						.filters(rw -> rw.rewritePath("/get_cart_by_ID/(?<UID>.*)", "/carts/${UID}"))
+						.uri("http://"+arg.get(0)+":8082"))
+				.route(p -> p
+						.path("/product_to_cart")
+						.and().method("POST")
+						.filters(rw -> rw.rewritePath("/product_to_cart", "/carts/products"))
+						.uri("http://"+arg.get(0)+":8081"))
+				.route(p -> p
+						.path("/update_product_quantity/**/**/**")
+						.and().method("PUT")
+						.filters(rw -> rw.rewritePath("/update_product_quantity/(?<UID>.*)/(?<PID>.*)/(?<Q>.*)", "/carts/${UID}/products/${PID}/${Q}"))
+						.uri("http://"+arg.get(0)+":8081"))
+				.route(p -> p
+						.path("/remove_cart_product/**/**")
+						.and().method("DELETE")
+						.filters(rw -> rw.rewritePath("/remove_cart_product/(?<UID>.*)/(?<PID>.*)", "/carts/${UID}/products/${PID}"))
+						.uri("http://"+arg.get(0)+":8081"))
+				.build();
+	}
+
 }
