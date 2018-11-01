47879dbaf845e38f2a85a56c0d09eb0ffe12fb2c
Richard <rr222ge@student.lnu.se>
2018-11-01 14:09:23 +0100

    Added Store Entity and ProductAdded event

--- store-domain-service/src/main/java/org/store/domain/service/dao/Store.java
+++
@@ -1,9 +0,0 @@
-package org.store.domain.service.dao;
-
-public class Store {
-	
-	private String id;
-	private String name;
-	private String website;
-	private String location;
-}
---
+++ store-domain-service/src/main/java/org/store/domain/service/model/Store.java
@@ -0,0 +1,69 @@
+package org.store.domain.service.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Store {
+	
+	@Id
+    @NotBlank
+    @Column(name = "id", nullable = false, unique = true)
+	private String id;
+	
+    @NotBlank
+    @Column(name = "name", nullable = false)
+	private String name;
+	
+    @NotBlank
+    @Column(name = "website", nullable = false)
+	private String website;
+	
+    @NotBlank
+    @Column(name = "location", nullable = false)
+	private String location;
+    
+	public Store(){
+		
+	}
+
+	public Store(String id, String name, String website, String location) {
+		this.setId(id);
+		this.setName(name);
+		this.setWebsite(website);
+		this.setLocation(location);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = (id == null) ? this.id : (id.isEmpty() ? this.id : id);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = (name == null) ? this.name : (name.isEmpty() ? this.name: name);
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = (website == null) ? this.website : (website.isEmpty() ? this.website: website);
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = (location == null) ? this.location : (location.isEmpty() ? this.location: location);
+	}	
+}
