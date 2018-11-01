16c896668d6c0d9b3b40269957fcd4d004642d65
Richard <rr222ge@student.lnu.se>
2018-11-01 14:12:12 +0100

    fix

--- store-domain-service/src/main/java/org/store/domain/service/model/Store.java
+++ store-domain-service/src/main/java/org/store/domain/service/model/Store.java
@@ -1,10 +1,14 @@
 package org.store.domain.service.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+@Entity
+@Table(name = "store")
 public class Store {
 	
 	@Id
---
+++ store-domain-service/src/main/java/org/store/domain/service/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package org.store.domain.service.repository;
+
+import org.store.domain.service.model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, String> {
+}
