2fee2b3024653b3e49b8a1275f48ca21ed01a61b
Richard <rr222ge@student.lnu.se>
2018-11-02 14:52:01 +0100

    package fixed

--- store-domain/src/main/java/org/store/domain/event/StoreEvent.java
+++ store-domain/src/main/java/org/store/domain/event/StoreEvent.java
@@ -3,7 +3,7 @@ package org.store.domain.event;
 import io.eventuate.Event;
 import io.eventuate.EventEntity;
 
-@EventEntity(entity = "org.cart.command.service.aggregate.StoreAggregate")
+@EventEntity(entity = "org.store.command.service.aggregate.StoreAggregate")
 public interface StoreEvent extends Event {
 
 }
