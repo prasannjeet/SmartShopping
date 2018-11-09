b03dc838fae88da1c571082fe8258d4d449b2cd6
Richard <rr222ge@student.lnu.se>
2018-11-09 12:45:51 +0100

    Subscriber listens to store events now

--- gateway-service/src/main/java/org/gateway/command/service/subscriber/Subscriber.java
+++ gateway-service/src/main/java/org/gateway/command/service/subscriber/Subscriber.java
@@ -2,11 +2,10 @@ package org.gateway.command.service.subscriber;
 
 import java.util.concurrent.Semaphore;
 
-import org.gateway.domain.event.GatewayEventAddProductInStore;
-import org.gateway.domain.event.GatewayEventInitializeStore;
-import org.gateway.domain.event.GatewayEventScrap;
-import org.gateway.domain.event.GatewayEventUpdatePriceInStore;
 import org.store.domain.event.StoreEventProductCreated;
+import org.store.domain.event.StoreEventProductPriceUpdated;
+import org.store.domain.event.StoreEventScrapperLaunched;
+import org.store.domain.event.StoreEventStoreCreated;
 
 import io.eventuate.DispatchedEvent;
 import io.eventuate.EventHandlerMethod;
@@ -20,30 +19,28 @@ public class Subscriber {
 	public Semaphore updateProductSemaphore = new Semaphore(0);
 	public Semaphore scrapStoreSemaphore = new Semaphore(0);
 	
+	public StoreEventStoreCreated storeInitEvent = null;
+	public StoreEventProductPriceUpdated addProductEvent = null;
+	public StoreEventProductCreated updateProductEvent = null;
+	public StoreEventScrapperLaunched scrapStoreEvent = null;
+	
 	@EventHandlerMethod
-	public void initializedStore(DispatchedEvent<GatewayEventInitializeStore> event){
+	public void initializedStoreResponse(DispatchedEvent<StoreEventStoreCreated> event){
 		storeInitSemaphore.release();
 	}
 	
 	@EventHandlerMethod
-	public void updateProductReponse(DispatchedEvent<GatewayEventUpdatePriceInStore> event){
+	public void updateProductReponse(DispatchedEvent<StoreEventProductPriceUpdated> event){
 		updateProductSemaphore.release();
 	}
 	
 	@EventHandlerMethod
-	public void addProductReponse(DispatchedEvent<GatewayEventAddProductInStore> event){
-		addProductSemaphore.release();
-	}
-	
-	/*
-	@EventHandlerMethod
 	public void addProductReponse(DispatchedEvent<StoreEventProductCreated> event){
 		addProductSemaphore.release();
 	}
-	*/
 	
 	@EventHandlerMethod
-	public void scrapStoreReponse(DispatchedEvent<GatewayEventScrap> event){
+	public void scrapStoreReponse(DispatchedEvent<StoreEventScrapperLaunched> event){
 		scrapStoreSemaphore.release();
 	}
 }
